db01703cae2beadcb6b11c6ee84a74a6d41067d2
Eduard Muliar <edikmulyar228@gmail.com>
2026-04-07 15:06:33 +0300

    feat: integrate Inngest for background workflow processing and add Workflow model to Prisma schema

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,20 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/prisma";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany();
+  getWorkflows: protectedProcedure.query(() => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async() => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "test@gmail.com",
+      },
+    });
+
+    return {success: true, message: "Workflow created successfully"}
   }),
 });
 // export type definition of API
